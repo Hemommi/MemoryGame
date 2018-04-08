8667940c2e069cd499af1936cabc45608282bf2c
Sandra Czerkas <sandra.czerkas@gmail.com>
2018-04-07 21:08:21 -0400

    matching logic

--- js/app.js
+++ js/app.js
@@ -1,32 +1,6 @@
 /*List that holds all cards*/
 	let cards = document.getElementsByClassName("card");
-	let dupa = [
-		"fas fa-chess-pawn",
-		"fas fa-chess-pawn",
-		"fas fa-bicycle",
-		"fas fa-bicycle",
-		"fas fa-anchor",
-		"fas fa-anchor",
-		"fas fa-camera-retro",
-		"fas fa-camera-retro",
-		"fas fa-car",
-		"fas fa-car",
-		"fas fa-binoculars",
-		"fas fa-binoculars",
-		"fas fa-birthday-cake",
-		"fas fa-birthday-cake",
-		"fas fa-bomb",
-		"fas fa-bomb"];
-
-	var scorePanel;
-	var stars;
-	var reloadButton;
-	var deck;
-	var memoryValues = [];
-	var flippedCards = 0;
-	var memoryCardsIds = [];
-
-
+	let card = [...cards];
 
 /* Add event listeners to each card. Set up the event listener for a card. If a card is clicked*/
 	
@@ -34,12 +8,46 @@
 		cards[i].addEventListener("click", cardClicked);
 	};
 
-	function cardClicked(parameter){
-		alert("Clicked");
-	}
+	function cardClicked(event){
+		var card = event.currentTarget;
+		var isOpen = $(card).hasClass("open");
 
+		if(isOpen){
+			//do nothing
+		}else {
 
-/*Cards displayed on the page/New Game*/
+			var openedCards = $(".open");
+			var matchedCards = $(".match");
+			//open card
+			$(card).addClass("open show");
+			
+			var clickedChild = $(card).children()[0];
+			var clickedChildClass = clickedChild.getAttribute("class");
+			var isMatched = false;
+		
+			for(var i = 0; i < openedCards.length; i++ ){
+				var openedCard = openedCards[i];
+				var openedCardChild = $(openedCard).children()[0];
+				var openedCardChildClass = openedCardChild.getAttribute("class");
+				
+				if(openedCardChildClass === clickedChildClass){
+					isMatched = true;
+					$(card).addClass("match");
+					$(openedCard).addClass("match");
+				}
+			}
+
+				if(isMatched === false && (openedCards.length - matchedCards.length )> 0){
+					$(card).removeClass("open show");
+				}
+				
+			}
+
+		
+		//alert("Clicked isOpen=" +isOpen);
+
+		var isShow = $(card).hasClass("show");
+			//alert("Clicked isShow=" +cards);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 	
@@ -59,65 +67,22 @@
 
 /* Deck cards in game*/
 
-	var deck = document.querySelector(".deck");
-	function start() {
-		var shuffleAllCards = shuffle(cards);
-		for (var i=0; i < shuffleAllCards; i++){
-		[].forEach.call(shuffleAllCards, function(){
-			deck.appendChild(card);
-		});
-	}
+	//var deck = document.querySelector(".deck");
+	//function start() {
+		//var shuffleAllCards = shuffle(cards);
+		//for (var i=0; i < shuffleAllCards; i++){
+		//[].forEach.call(shuffleAllCards, function(){
+			//deck.appendChild(card);
+		//});
+	//}
 
-}
-/*
- * :
- *  - display the card's symbol (put this functionality in another function that you call from this one)
-/*
- * Add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- * if the list already has another card, check to see if the two cards match
- */
-	function cardOpen() {
-		openedCard.push(this);
-		var cardList = openCards.length;
-		if(cardList === 2) {
-			moveCounter();
-			if(openedCards[0].type ===openedCards[1].type){
-				matched();
-			} else {
-				unmached();
-			}
-		}
-	}; 
-/*
- *if the two cards match
- */
-function matched (){
-	openedCards[0].classList.add("match");
-	openedCards[1].classLIst.add("match");
-	openedCards[0].classList.remove("show", "open");
-	openedCards[1].classLIst.remove("show", "open");
-	openedCards = [];
-	};
-/*
- *If the cards do not match
- */
-function unmatched(){
-	openedCards[0].classList.add("unmatched");
-	openedCards[1].classList.add("unmatched");
-	openedCards[0].classList.remove("show", "open", "hide");
-	openedCards[1].classList.remove("show", "open", "hide");
-	closedCards = [];
-};
-/*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-/*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-
- /*Reset button*/
+/*Reset button*/
 	document.getElementsByClassName("reload-button").onclick = function (){
 		document.getElementsByClassName("reload-button").reset();
 	};
 
 /*Score panel*/
-	var scorePanel = getElementsByClassName(score-panel);
+	var scorePanel = getElementsByClassName("score-panel");
 	var i;
 	function scorePanel(){
 		scorePanel.innerHTML = moves;
@@ -132,4 +97,5 @@ function unmatched(){
 				stars.style.display = "collapse";
 			}
 		}
-	};
+	}
+}
